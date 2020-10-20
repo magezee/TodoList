95652e7ba6339cbd2b9e818b91380ed458f8097e
magezee <756591143@qq.com>
2020-10-21 06:31:16 +0800

    add component

--- list/src/store/index.ts
+++ list/src/store/index.ts
@@ -7,7 +7,7 @@ import userReducer from './user/reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 const rootReducer = combineReducers({
-    userReducer,
+    user: userReducer,
 });
 
 export const store = createStore(
@@ -15,6 +15,8 @@ export const store = createStore(
     composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
+// 这里的AppStore实际上是combineReducers中所有state的并类型，相当于{ user: IUserState, xxx:XXX ... }(注意这里是一个对象包含所有类型)
+// 这样写的好处是可以解构出对应的state，然后直接提示该state的所有属性
 export type AppStore = ReturnType<typeof rootReducer>;
 
 sagaMiddleware.run(rootSaga);
--- list/src/views/Index/index.tsx
+++ list/src/views/Index/index.tsx
@@ -1,13 +1,24 @@
-import React, { FC, useEffect, useState } from 'react';
-import { Button } from 'antd';
+import React, { FC, useEffect, useState } from "react";
+import { withRouter, RouteComponentProps } from "react-router"; // TS需要引入传入的props类型
+import { connect, ConnectedProps } from "react-redux"; // TS需要引入传入的props类型
 
+import { LocalStorage } from "../../utils";
+import { AppStore } from "../../store";
+import { keepLogin } from "../../store/user/action";
 
-const Index: FC<any> = () => {
-    return (
-        <div>
-            <Button>首页</Button>
-        </div>
-    )
+// 对redux的映射
+const mapState = ({ user }: AppStore) => ({
+  user,
+});
+const mapDispatch = {
+  keepLogin,
 };
 
-export default Index
+const connector = connect(mapState, mapDispatch);
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+const Home: FC<RouteComponentProps> = () => {
+  return <div></div>;
+};
+
+export default Home;
